e555a8020d21f114c873b6fa826909a8fc8daee5
Alex Edwards <alextcedwards@gmail.com>
2021-08-20 08:53:45 -0600

    all tests passing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const { add } = require('./auth-model')
+const { checkCredentials, checkUserExists } = require('../middleware/checkUser')
+const { tokenBuilder } = require('../middleware/tokenBuilder')
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+router.post('/register', checkCredentials, (req, res) => {
+  
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -27,10 +31,24 @@ router.post('/register', (req, res) => {
     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
+      const credentials = req.body;
+      const hash = bcrypt.hashSync(credentials.password, 8)
+      credentials.password = hash
+    
+      add(credentials)
+        .then((user) => {
+          res.status(201).json(user)
+        })
+        .catch((err) => {
+          res.status(400).json({
+            message: 'username and password required',
+            error: err.message
+          })
+        })
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login',  checkUserExists, (req, res) => {
+  
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -54,6 +72,21 @@ router.post('/login', (req, res) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
-});
+      const user = req.body.user;
+      const { username, password } = req.body;
+    
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenBuilder(user);
+        res.status(200).json({
+          message: `welcome, ${username} `,
+          token: token,
+        });
+      } else {
+        res.status(401).json({
+          message: "invalid credentials",
+        });
+      }
+    });
+      
 
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,108 @@
 // Write your tests here
+
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const server = require('./server')
+
+const user = {
+  username: "Captain Marvel",
+  password: "foobar"
+};
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async () => {
+  await db('users').truncate()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+
+describe('Test endpoints', () => {
+  describe("[POST] /api/auth/register", () => {
+    test("user can register", async () => {
+      let res;
+      res = await request(server).post("/api/auth/register").send(user);
+      expect(res.status).toBe(201);
+    });
+    it("adds the user to the database", async () => {
+      let res;
+      await request(server).post("/api/auth/register").send(user);
+      res = await db('users')
+        .where({ id: 1 })
+        .first();
+      expect(res.username).toBe(user.username);
+    });
+    it("rejects username if taken", async () => {
+      let res;
+      await request(server).post("/api/auth/register").send(user);
+
+      res = await request(server).post("/api/auth/register").send(user);
+      expect(res.status).toBe(401);
+      expect(res.text).toMatch(/.*username taken.*/);
+    });
+    it("requires username and password", async () => {
+      let res;
+      res = await request(server)
+        .post("/api/auth/register")
+        .send({password: "foobar"});
+      expect(res.text).toMatch(/.*username and password required.*/);
+
+      res = await request(server)
+        .post("/api/auth/register")
+        .send({username: "Captain Marvel"});
+      expect(res.text).toMatch(/.*username and password required.*/);
+    });
+  describe("[POST] /api/auth/login", () => {
+    it("requires username and password", async () => {
+      let res;
+      res = await request(server)
+        .post("/api/auth/login")
+        .send({password: "foobar"});
+      expect(res.text).toMatch(/.*username and password required.*/);
+
+      res = await request(server)
+        .post("/api/auth/login")
+        .send({username: "Captain Marvel"});
+      expect(res.text).toMatch(/.*username and password required.*/);
+    });
+    it("checks if username exists", async () => {
+      let res;
+      res = await request(server)
+        .post("/api/auth/login")
+        .send(user);
+        expect(res.text).toMatch(/.*invalid credentials.*/);
+    })
+    it("checks if password is correct", async () => {
+      let res;
+      await request(server)
+        .post("/api/auth/register")
+        .send(user);
+      res = await request(server)
+        .post("/api/auth/login")
+        .send({...user, password: "bad_password"});
+        expect(res.text).toMatch(/.*invalid credentials.*/);
+    });
+    it("logs in with valid credentials", async () => {
+      let res;
+      await request(server)
+        .post("/api/auth/register")
+        .send(user);
+      res = await request(server)
+        .post("/api/auth/login")
+        .send(user);
+      expect(res.text).toMatch(/.*welcome.*token.*/);
+    });
+  });
+  })
 })
